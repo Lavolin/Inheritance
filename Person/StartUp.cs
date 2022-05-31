b538b0921903d0068ebe648afc2703dd7a28195e
Todor Stoyanov <lavolin@gmail.com>
2022-05-31 18:08:49 +0300

    31.05.18.08

--- Person/StartUp.cs
+++ Person/StartUp.cs
@@ -4,7 +4,45 @@
     {
         public static void Main(string[] args)
         {
+            string name = Console.ReadLine();
+            int age = int.Parse(Console.ReadLine());
+            
+            if (age <= 15)
+            {
+                Child child = new Child(name, age);
+                Console.WriteLine(child);
+            }
+            else
+            {
+                Person person = new Person(name, age);
+                Console.WriteLine(person);
+                
+            }
 
         }
+    }
+}
+
+namespace Person
+{
+    public class Person
+    {
+        // 1. Add Fields
+
+        // 2. Add Constructor
+
+        // 3. Add Properties
+
+        // 4. Add Methods
+
+    }
+}
+
+namespace Child : Person
+{
+    public class Child
+    {
+       
+
     }
 }
