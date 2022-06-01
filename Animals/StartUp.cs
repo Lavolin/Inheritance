1ec19bab076c906567c27208745b74ebad72615c
Lavolin <lavolin@gmail.com>
2022-06-01 23:29:45 +0300

    01.06.23.29

--- Animals/StartUp.cs
+++ Animals/StartUp.cs
@@ -9,13 +9,48 @@ namespace Animals
             while (true)
             {
                 string type = Console.ReadLine();
-                if (type == "Beast")
+
+                if (type == "Beast!")
                 {
                     break;
                 }
 
                 string[] animalInfo = Console.ReadLine().Split();
+                string name = animalInfo[0];
+                int age = int.Parse(animalInfo[1]);
 
+                if (age < 0 )
+                {
+                    Console.WriteLine("Invalid input!");
+                    continue;
+                }
+
+                Animal animal = default; // = null;
+
+                if (type == "Dog")
+                {
+                    animal = new Dog(name, age, animalInfo[2]);
+                }
+                else if (type == "Cat")
+                {
+                    animal = new Cat(name, age, animalInfo[2]);
+                }
+                else if (type == "Frog")
+                {
+                    animal = new Frog(name, age, animalInfo[2]);
+                }
+                else if (type == "Kitten")
+                {
+                    animal = new Kitten(name, age);
+                }
+                else if (type == "Tomcat")
+                {
+                    animal = new Tomcat(name, age);
+                }
+
+                Console.WriteLine(type);
+                Console.WriteLine($"{animal.Name} {animal.Age} {animal.Gender}");
+                Console.WriteLine(animal.ProduceSound());
             }
         }
     }
